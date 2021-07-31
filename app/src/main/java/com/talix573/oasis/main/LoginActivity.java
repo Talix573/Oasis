742c7d2dac1d66ba265c34a7b7c6c9c708dad0d8
Kazor Vinci <kazorvinci@gmail.com>
2021-08-01 03:05:07 +0800

    Fixed conflicts

--- app/src/main/java/com/talix573/oasis/main/LoginActivity.java
+++ app/src/main/java/com/talix573/oasis/main/LoginActivity.java
@@ -3,12 +3,12 @@ package com.talix573.oasis.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
-<<<<<<< Updated upstream:app/src/main/java/com/talix573/oasis/main/LoginActivity.java
 
 import com.talix573.oasis.R;
-=======
->>>>>>> Stashed changes:app/src/main/java/com/talix573/oasis/LoginActivity.java
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,31 +17,10 @@ public class LoginActivity extends AppCompatActivity {
         accounts from the MainActivity. This handles logins using Firebase Authentication.
      */
 
-    private TextView testTextView;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        testTextView = this.findViewById(R.id.TestTextView);
-        testTextView.setText("Login");
-    }
-
-}
-
-    public LoginActivity() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
 }
--- app/src/main/java/com/talix573/oasis/main/RegisterActivity.java
+++ app/src/main/java/com/talix573/oasis/main/RegisterActivity.java
@@ -3,21 +3,18 @@ package com.talix573.oasis.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.talix573.oasis.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    public RegisterActivity() {
-        // Required empty public constructor
-    }
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
 
 }
