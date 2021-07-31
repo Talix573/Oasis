44cf5c350ac874da5227846520156a017662b51f
Talix573 <goh.yanzhen@gmail.com>
2021-08-01 02:45:04 +0800

    Login and register UI done

--- app/src/main/java/com/talix573/oasis/main/LoginActivity.java
+++ app/src/main/java/com/talix573/oasis/main/LoginActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+<<<<<<< Updated upstream:app/src/main/java/com/talix573/oasis/main/LoginActivity.java
 
 import com.talix573.oasis.R;
+=======
+>>>>>>> Stashed changes:app/src/main/java/com/talix573/oasis/LoginActivity.java
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,9 +17,31 @@ public class LoginActivity extends AppCompatActivity {
         accounts from the MainActivity. This handles logins using Firebase Authentication.
      */
 
+    private TextView testTextView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        testTextView = this.findViewById(R.id.TestTextView);
+        testTextView.setText("Login");
     }
+
+}
+
+    public LoginActivity() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
 }
--- app/src/main/java/com/talix573/oasis/main/RegisterActivity.java
+++ app/src/main/java/com/talix573/oasis/main/RegisterActivity.java
@@ -8,9 +8,16 @@ import com.talix573.oasis.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+    public RegisterActivity() {
+        // Required empty public constructor
     }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
 }
