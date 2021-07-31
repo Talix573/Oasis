93353e38628c5f16910de60b6a73ed75ae553310
Kazor Vinci <kazorvinci@gmail.com>
2021-07-31 17:08:52 +0800

    Initial Commit

---
+++ app/src/main/java/com/talix573/oasis/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.talix573.oasis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    /*
+        This activity will be launched if user is detected to be not logged in into any
+        accounts from the MainActivity. This handles logins using Firebase Authentication.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/talix573/oasis/PlantDetailsFragment.java
@@ -0,0 +1,47 @@
+package com.talix573.oasis;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.talix573.oasis.databinding.FragmentPlantDetailsBinding;
+
+public class PlantDetailsFragment extends Fragment {
+
+    private FragmentPlantDetailsBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentPlantDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(PlantDetailsFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
